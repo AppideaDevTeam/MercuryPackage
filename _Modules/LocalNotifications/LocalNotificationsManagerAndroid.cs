8083d8348d699bf160f6074925c6a295c0c658f6
MadFish <madfishgamesofficial@gmail.com>
2022-11-11 15:06:13 +0400

    Added limit to amount of notifications that can be scheduled at once on android

--- _Modules/LocalNotifications/LocalNotificationsManagerAndroid.cs
+++ _Modules/LocalNotifications/LocalNotificationsManagerAndroid.cs
@@ -9,34 +9,51 @@ namespace Mercury.LocalNotifications
 {
     internal class LocalNotificationsManagerAndroid : ILocalNotificationsManager_Platform
     {
+        private const int MAX_NOTIF_COUNT = 500;
+
         #region IMPLEMENTATIONS
+
+        private int notifCount = 0;
+
         public void Initialize()
         {
             InitializeChannels();
-        } 
-        
+        }
+
         public void ScheduleNotification(LocalNotification _notification)
         {
+            if (notifCount + 1 == MAX_NOTIF_COUNT)
+            {
+                MercuryDebugger.LogMessage(LogModule.LocalNotifications, "Maximum notification limit has been reached. (Skipping)", LogType.Error);
+                return;
+            }
+
             var notification = new AndroidNotification()
             {
                 Title = _notification.Title,
                 Text = _notification.Text,
                 FireTime = DateTime.Now.Add(_notification.FireTimeDelay),
-                ShowTimestamp = true,
-                Color = _notification.IconColor
+                ShowTimestamp = true
             };
 
             string targetChannelID = string.IsNullOrEmpty(_notification.ChannelID) ? MercuryLibrarySO.LocalNotificationsDatabase.Channel_Default.ID : _notification.ChannelID;
-            
+
             if (!string.IsNullOrEmpty(_notification.IconSmall)) notification.SmallIcon = _notification.IconSmall;
             if (!string.IsNullOrEmpty(_notification.IconLarge)) notification.LargeIcon = _notification.IconLarge;
             if (!string.IsNullOrEmpty(_notification.Data)) notification.IntentData = _notification.Data;
 
             AndroidNotificationCenter.SendNotification(notification, targetChannelID);
+
+            notifCount++;
         }
-        
-        public void CancelAllNotifications() => AndroidNotificationCenter.CancelAllNotifications();
-        
+
+        public void CancelAllNotifications()
+        {
+            AndroidNotificationCenter.CancelAllNotifications();
+
+            notifCount = 0;
+        }
+
         public bool ApplicationLaunchedViaNotification(out string data)
         {
             var intentData = AndroidNotificationCenter.GetLastNotificationIntent();
@@ -53,18 +70,18 @@ namespace Mercury.LocalNotifications
             return false;
         }
         #endregion
-        
+
         private static void InitializeChannels()
         {
             // STATIC CHANNELS
             List<NotificationChannel> channelsToInitialize = new List<NotificationChannel>() { MercuryLibrarySO.LocalNotificationsDatabase.Channel_Default };
 
-            if(MercuryLibrarySO.LocalNotificationsDatabase.RemindersEnabled) channelsToInitialize.Add(MercuryLibrarySO.LocalNotificationsDatabase.Channel_Reminders);
-            if(MercuryLibrarySO.LocalNotificationsDatabase.PeriodicRewardsEnabled) channelsToInitialize.Add(MercuryLibrarySO.LocalNotificationsDatabase.Channel_PeriodicRewards);
-            if(MercuryLibrarySO.LocalNotificationsDatabase.FreeResourcesEnabled) channelsToInitialize.Add(MercuryLibrarySO.LocalNotificationsDatabase.Channel_FreeResources);
-            if(MercuryLibrarySO.LocalNotificationsDatabase.ProcessesEnabled) channelsToInitialize.Add(MercuryLibrarySO.LocalNotificationsDatabase.Channel_Processes);
-            if(MercuryLibrarySO.LocalNotificationsDatabase.EnemyConnectedEnabled) channelsToInitialize.Add(MercuryLibrarySO.LocalNotificationsDatabase.Channel_EnemyConnected);
-            
+            if (MercuryLibrarySO.LocalNotificationsDatabase.RemindersEnabled) channelsToInitialize.Add(MercuryLibrarySO.LocalNotificationsDatabase.Channel_Reminders);
+            if (MercuryLibrarySO.LocalNotificationsDatabase.PeriodicRewardsEnabled) channelsToInitialize.Add(MercuryLibrarySO.LocalNotificationsDatabase.Channel_PeriodicRewards);
+            if (MercuryLibrarySO.LocalNotificationsDatabase.FreeResourcesEnabled) channelsToInitialize.Add(MercuryLibrarySO.LocalNotificationsDatabase.Channel_FreeResources);
+            if (MercuryLibrarySO.LocalNotificationsDatabase.ProcessesEnabled) channelsToInitialize.Add(MercuryLibrarySO.LocalNotificationsDatabase.Channel_Processes);
+            if (MercuryLibrarySO.LocalNotificationsDatabase.EnemyConnectedEnabled) channelsToInitialize.Add(MercuryLibrarySO.LocalNotificationsDatabase.Channel_EnemyConnected);
+
             // CUSTOM CHANNELS
             if (!MercuryLibrarySO.LocalNotificationsDatabase.Channels_Custom.IsNullOrEmpty()) channelsToInitialize.AddRange(MercuryLibrarySO.LocalNotificationsDatabase.Channels_Custom);
 
@@ -74,12 +91,12 @@ namespace Mercury.LocalNotifications
                 {
                     var newChannel = new AndroidNotificationChannel()
                     {
-                        Id          = channel.ID,
-                        Name        = channel.Name,
+                        Id = channel.ID,
+                        Name = channel.Name,
                         Description = channel.Description,
-                        Importance  = Importance.Default
+                        Importance = Importance.Default
                     };
-                    
+
                     AndroidNotificationCenter.RegisterNotificationChannel(newChannel);
                 }
             }
